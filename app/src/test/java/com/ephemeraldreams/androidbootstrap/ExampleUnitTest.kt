1f9679cc03bc47888db6f55bcce95ee91fdd1e5a
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-01-17 15:03:06 -0800

    Make test imports explicit.

--- app/src/test/java/com/ephemeraldreams/androidbootstrap/ExampleUnitTest.kt
+++ app/src/test/java/com/ephemeraldreams/androidbootstrap/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.ephemeraldreams.androidbootstrap
 
+import kotlin.test.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
