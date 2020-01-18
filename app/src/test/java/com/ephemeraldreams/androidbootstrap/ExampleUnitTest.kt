9d348246f03bc0848d887fe12757f36fcbfa767a
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-01-17 17:36:23 -0800

    Format Kotlin files with spotless.

--- app/src/test/java/com/ephemeraldreams/androidbootstrap/ExampleUnitTest.kt
+++ app/src/test/java/com/ephemeraldreams/androidbootstrap/ExampleUnitTest.kt
@@ -3,7 +3,6 @@ package com.ephemeraldreams.androidbootstrap
 import kotlin.test.assertEquals
 import org.junit.Test
 
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
