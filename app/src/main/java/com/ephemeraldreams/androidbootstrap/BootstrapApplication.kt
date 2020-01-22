111049dc08d83d8716ba54660da8bd3ae86b4c72
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-01-21 16:10:17 -0800

    Set up Dagger and OkHttp dependencies.
    Configure Dagger to provide an application context, application id, and
    an OkHttpClient for injection to start with.

--- app/src/main/java/com/ephemeraldreams/androidbootstrap/BootstrapApplication.kt
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/BootstrapApplication.kt
@@ -1,14 +1,23 @@
 package com.ephemeraldreams.androidbootstrap
 
-import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import javax.inject.Inject
+import okhttp3.OkHttpClient
 import timber.log.Timber
 
-class BootstrapApplication : Application() {
+class BootstrapApplication : DaggerApplication() {
+
+    @Inject lateinit var okHttpClient: OkHttpClient
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerApplicationComponent.factory().inject(this)
     }
 }
---
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/annotations/ApplicationId.kt
@@ -0,0 +1,8 @@
+package com.ephemeraldreams.androidbootstrap.annotations
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+@MustBeDocumented
+annotation class ApplicationId
