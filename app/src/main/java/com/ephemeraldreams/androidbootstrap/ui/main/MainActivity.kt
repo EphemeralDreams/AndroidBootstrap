e39f92a5257761769e06893d3de28659904154d8
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-01-28 22:56:46 -0800

    Move MainActivity to its own package

--- app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/main/MainActivity.kt
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.ephemeraldreams.androidbootstrap
+package com.ephemeraldreams.androidbootstrap.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
+import com.ephemeraldreams.androidbootstrap.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
