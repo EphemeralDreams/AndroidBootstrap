7c3512974313084fd687df91c584e0c64fb4e5dd
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-03-02 15:07:21 -0800

    Enable and use view binding

--- app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/home/HomeActivity.kt
@@ -1,21 +1,21 @@
 package com.ephemeraldreams.androidbootstrap.ui.home
 
 import android.os.Bundle
-import android.widget.TextView
-import com.ephemeraldreams.androidbootstrap.R
 import com.ephemeraldreams.androidbootstrap.annotations.ApplicationId
+import com.ephemeraldreams.androidbootstrap.databinding.ActivityHomeBinding
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class HomeActivity : DaggerAppCompatActivity() {
 
     @Inject @ApplicationId lateinit var applicationId: String
+    private lateinit var homeBinding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        homeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
 
-        val textView = findViewById<TextView>(R.id.home_text_view)
-        textView.text = applicationId
+        homeBinding.homeTextView.text = applicationId
     }
 }
