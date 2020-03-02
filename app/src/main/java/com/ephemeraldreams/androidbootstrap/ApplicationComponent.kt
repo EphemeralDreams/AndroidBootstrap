4482a212f03df6ae8b668d57e72483409c412bc3
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-03-02 15:03:10 -0800

    Set up Dagger DI for HomeActivity

--- app/src/main/java/com/ephemeraldreams/androidbootstrap/ApplicationComponent.kt
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.ephemeraldreams.androidbootstrap
 
 import com.ephemeraldreams.androidbootstrap.data.DataModule
 import com.ephemeraldreams.androidbootstrap.net.NetworkModule
+import com.ephemeraldreams.androidbootstrap.ui.ActivityModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
@@ -12,6 +13,7 @@ import javax.inject.Singleton
 @Component(
     modules = [
         AndroidInjectionModule::class,
+        ActivityModule::class,
         ApplicationModule::class,
         DataModule::class,
         NetworkModule::class
--- app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/home/HomeActivity.kt
@@ -1,6 +1,21 @@
 package com.ephemeraldreams.androidbootstrap.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
 import com.ephemeraldreams.androidbootstrap.R
+import com.ephemeraldreams.androidbootstrap.annotations.ApplicationId
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
-class HomeActivity : AppCompatActivity(R.layout.activity_home)
+class HomeActivity : DaggerAppCompatActivity() {
+
+    @Inject @ApplicationId lateinit var applicationId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val textView = findViewById<TextView>(R.id.home_text_view)
+        textView.text = applicationId
+    }
+}
