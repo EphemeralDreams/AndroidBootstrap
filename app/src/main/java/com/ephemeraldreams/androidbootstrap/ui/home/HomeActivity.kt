b6c6bdf89de4e5b345ce1ef49972dcd955a86492
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-03-02 14:51:46 -0800

    Rename MainActivity to HomeActivity and move to ui package

---
+++ app/src/main/java/com/ephemeraldreams/androidbootstrap/ui/home/HomeActivity.kt
@@ -0,0 +1,6 @@
+package com.ephemeraldreams.androidbootstrap.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import com.ephemeraldreams.androidbootstrap.R
+
+class HomeActivity : AppCompatActivity(R.layout.activity_home)
