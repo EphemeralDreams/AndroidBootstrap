91c546c00d91f49559b07b008a0169bec7afb46b
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-03-16 22:58:50 -0700

    Add utility functions to get git metadata

---
+++ buildSrc/src/main/kotlin/com/ephemeraldreams/Git.kt
@@ -0,0 +1,31 @@
+package com.ephemeraldreams
+
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+object Git {
+    fun ref(): String = System.getenv("GITHUB_REF") ?: "git rev-parse --abbrev-ref HEAD".cmd()
+    fun sha(): String = System.getenv("GITHUB_SHA") ?: "git rev-parse HEAD".cmd()
+}
+
+/**
+ * Execute a shell command.
+ * @param workingDir The working directory where to run the shell command.
+ * @param timeout The timeout represented in seconds.
+ */
+private fun String.cmd(workingDir: File = File("."), timeout: Long = 60 * 10): String {
+    return try {
+        val cmdParts = this.split("\\s".toRegex()).toTypedArray()
+        val process = ProcessBuilder(*cmdParts)
+            .directory(workingDir)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+        process.waitFor(timeout, TimeUnit.SECONDS)
+        process.inputStream.bufferedReader().readText().trim()
+    } catch (e: IOException) {
+        e.printStackTrace()
+        ""
+    }
+}
