8f4f632bc51e794f6053dfede006bbb1fbaca0a8
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-03-20 21:09:34 -0700

    Update dependencyUpdates task to reject release candidates

--- buildSrc/src/main/kotlin/com/ephemeraldreams/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/ephemeraldreams/Dependencies.kt
@@ -15,11 +15,8 @@ object Versions {
      * Check whether [version] is not a stable version.
      */
     fun isNonStable(version: String): Boolean {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any {
-            version.toUpperCase(Locale.ROOT).contains(it)
-        }
         val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-        val isStable = stableKeyword || regex.matches(version)
+        val isStable = regex.matches(version)
         return isStable.not()
     }
 }
