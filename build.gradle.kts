2e947e35cc8f446b317405d84767d68c152d7a13
Ephemeral Dreamer <ephemeraldreamer100@gmail.com>
2020-04-17 11:57:42 -0700

    Exclude generated files from code styling checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,14 +34,17 @@ tasks.withType<DependencyUpdatesTask> {
 spotless {
     java {
         target("**/*.java")
+        targetExclude("**/build/**")
         googleJavaFormat(Versions.googleJavaFormat)
     }
     kotlin {
         target("**/*.kt")
+        targetExclude("**/build/**")
         ktlint(Versions.klint)
     }
     kotlinGradle {
         target("**/*.kts")
+        targetExclude("**/build/**")
         ktlint(Versions.klint)
     }
 }
